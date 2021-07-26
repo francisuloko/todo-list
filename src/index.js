173a7493d0873af8559a62404da4221ed1cc4b31
Francis Uloko <francisuloko@yahoo.com>
2021-07-26 18:06:12 +0100

    Fix linters error

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style.css';
 import checkboxesEvent from './status-update.js';
 import dragAndDrop from './drag-and-drop.js';
 import {
-  displayTasks, edit, clear, add
+  displayTasks, edit, clear, add,
 } from './crudtodo.js';
 
 displayTasks();
