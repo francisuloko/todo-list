afa7c1659a88f0a29ecaef1567788b779e072d2c
Francis Uloko <francisuloko@yahoo.com>
2021-07-29 12:44:53 +0100

    Add mock DOM module

--- __tests__/add-and-remove.test.js
+++ __tests__/add-and-remove.test.js
@@ -2,21 +2,16 @@
  * @jest-environment jsdom
  */
 
-import MockStorage from '../src/__mocks__/mock-storage.js';
+import MockStorage from '../src/__mocks__/local-storage.js';
+import MockDOM from '../src/__mocks__/DOM.js';
+import { add } from '../src/crudtodo.js'
 
 describe('Add Function', () => {
-  document.body.innerHTML = `
-       <form>
-         <input type="text" name="task-entry" id="task-entry" />
-       </form>
-       <div id="todo-list"></div>`;
-
   describe('Add new item to LocalStorage', () => {
     const mockStorage = new MockStorage();
+    const mockDOM = new MockDOM();
     const list = mockStorage.getList();
     const entryValue = document.getElementById('task-entry');
-    entryValue.value = 'Sample task 1';
-
     const event = new KeyboardEvent('keydown', {
       code: 'Enter',
       key: 'Enter',
@@ -25,6 +20,9 @@ describe('Add Function', () => {
       view: window,
       bubbles: true,
     });
+    entryValue.value = 'Sample task 1';
+
+    add();
 
     if (entryValue.dispatchEvent(event)) {
       list.push({
@@ -33,6 +31,8 @@ describe('Add Function', () => {
         index: list.length,
       });
     }
+
+    mockDOM.displayTask(list);
 
     test('new task added successfully', () => {
       expect(list.length).toBe(1);
@@ -52,13 +52,6 @@ describe('Add Function', () => {
   });
 
   describe('Display task items from LocalStorage', () => {
-    document.body.innerHTML = `
-       <form>
-         <input type="text" name="task-entry" id="task-entry" />
-       </form>
-      <div id="todo-list"></div>`;
-
-    const mockStorage = new MockStorage();
     const temp = [
       {
         description: 'Sample 1',
@@ -76,32 +69,12 @@ describe('Add Function', () => {
         index: 3,
       },
     ];
+    const mockDOM = new MockDOM();
+    const mockStorage = new MockStorage();
     mockStorage.setList(temp);
     const list = mockStorage.getList();
-    const todoList = document.getElementById('todo-list');
 
-    list.forEach((task) => {
-      let todoObj = '';
-      if (task.completed === true) {
-        todoObj = `
-            <article id="${task.index}" class="task-item" draggable="true">
-              <input type='checkbox' name='completed' class="checkbox" checked>
-              <span class='task-description completed' id="desc-${task.index}" contenteditable>${task.description}</span>
-              <i class="bi bi-three-dots-vertical"></i>
-              <i class="bi bi-trash hide"></i>
-            </article>`;
-      } else {
-        todoObj = `
-              <article  id="${task.index}" class="task-item" draggable="true">
-                <input type='checkbox' name='completed' class="checkbox">
-                <span class='task-description' id="desc-${task.index}" contenteditable>${task.description}</span>
-                <i class="bi bi-three-dots-vertical"></i>
-                <i class="bi bi-trash hide"></i>
-              </article>`;
-      }
-
-      todoList.innerHTML += todoObj;
-    });
+    mockDOM.displayTask(list);
 
     test('Items added to DOM', () => {
       const items = document.querySelectorAll('[draggable]');
@@ -111,12 +84,7 @@ describe('Add Function', () => {
 });
 
 describe('Remove task items from LocalStorage', () => {
-  document.body.innerHTML = `
-   <form>
-     <input type="text" name="task-entry" id="task-entry" />
-   </form>
-  <div id="todo-list"></div>`;
-
+  const mockDOM = new MockDOM();
   const mockStorage = new MockStorage();
   const temp = [
     {
@@ -137,32 +105,13 @@ describe('Remove task items from LocalStorage', () => {
   ];
   mockStorage.setList(temp);
   const list = mockStorage.getList();
-  const todoList = document.getElementById('todo-list');
 
-  list.forEach((task) => {
-    let todoObj = '';
-    if (task.completed === true) {
-      todoObj = `
-        <article id="${task.index}" class="task-item" draggable="true">
-          <input type='checkbox' name='completed' class="checkbox" checked>
-          <span class='task-description completed' id="desc-${task.index}" contenteditable>${task.description}</span>
-          <i class="bi bi-three-dots-vertical"></i>
-          <i class="bi bi-trash hide"></i>
-        </article>`;
-    } else {
-      todoObj = `
-          <article  id="${task.index}" class="task-item" draggable="true">
-            <input type='checkbox' name='completed' class="checkbox">
-            <span class='task-description' id="desc-${task.index}" contenteditable>${task.description}</span>
-            <i class="bi bi-three-dots-vertical"></i>
-            <i class="bi bi-trash hide"></i>
-          </article>`;
-    }
+  mockDOM.displayTask(list);
 
-    todoList.innerHTML += todoObj;
-  });
   test('Remove item from the list', () => {
+    const todoList = document.getElementById('todo-list');
     const items = document.querySelectorAll('.task-item');
+
     list.splice(items[0].id, 1);
     todoList.removeChild(todoList.children[0]);
 
